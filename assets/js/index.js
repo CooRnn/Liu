0b6cfd222c618c99ced7f8498848172ff709356e
CooRnn <ngocorn87@gmail.com>
2022-03-05 02:07:23 +0700

    Add style.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,10 @@ const closeMan4 = document.querySelector('.js-control-forward');
 
 const man1 = document.querySelector('.js-m1');
 
+const man2 = document.querySelector('.js-m2');
+const closeMan2 = document.querySelector('.js-m2-btn');
+
+// màn 4 
 function showM4() {
     man4.classList.add('open');
 }
@@ -16,8 +20,20 @@ unlockMan3.addEventListener('click', showM4);
 
 closeMan4.addEventListener('click', offM4);
 
+// màn 1
 function showM1() {
     man1.classList.add('close');
 }
 
 man1.addEventListener('click', showM1);
+
+// màn 2
+ function offM2() {
+     man2.classList.add('close');
+ }
+
+ closeMan2.addEventListener('click', offM2);
+
+
+
+
